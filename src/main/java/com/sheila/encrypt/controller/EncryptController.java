6037daacc9871e93dba78f24b67a5c0f5adae368
Sheila Schlickmann <sheila31sl@hotmail.com>
2022-09-22 21:23:40 -0300

    criação do encrypy-supremo

--- src/main/java/com/sheila/encrypt/controller/EncryptController.java
+++ src/main/java/com/sheila/encrypt/controller/EncryptController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/encrypt")
+@RequestMapping("/encrypt-supremo")
 public class EncryptController {
     final EncryptService encryptService;
 
